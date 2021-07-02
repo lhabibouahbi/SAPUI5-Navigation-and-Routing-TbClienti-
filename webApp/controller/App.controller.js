e010b8624603feed5424caa6c4a838176d14a08a
el habib ouahbi <elhabibouahbi@gmail.com>
2021-07-02 09:35:47 +0200

    Add files via upload

---
+++ webApp/controller/App.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.clienti.controller.App", {
+
+    });
+ });
