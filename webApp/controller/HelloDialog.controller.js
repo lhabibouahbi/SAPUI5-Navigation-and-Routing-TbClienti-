3b146618fad4a45963109b8e1006ac84c26ad510
el habib ouahbi <elhabibouahbi@gmail.com>
2021-07-02 09:41:53 +0200

    Add files via upload

---
+++ webApp/controller/HelloDialog.controller.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.clienti.controller.HelloPanel", {
+		onOpenDialog : function () {
+			var oView = this.getView();
+
+			// create dialog lazily
+			if (!this.pDialog) {
+				this.pDialog = Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.clienti.view.HelloDialog"
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					return oDialog;
+				});
+			} 
+			this.pDialog.then(function(oDialog) {
+				oDialog.open();
+			});
+		}
+
+	});
+});
